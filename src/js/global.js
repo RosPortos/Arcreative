60b44945b44265b4afabd8ab6322529d946a4c82
WebDev <nehaiki@ukr.net>
2023-01-30 15:12:03 +0200

    add new page

--- src/js/global.js
+++ src/js/global.js
@@ -1,30 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    function headerHide() {
-        const header = document.querySelector(".header");
-        let scrollPrev = 0;
 
-        window.addEventListener('scroll', function () {
-            let scrolled = scrollY;
-
-            if (scrolled > 10) {
-                header.classList.add('header-black');
-            } else {
-                header.classList.remove('header-black');
-            }
-
-            if (scrolled > 0 && scrolled > scrollPrev) {
-                header.classList.add('header-top');
-            } else {
-                header.classList.remove('header-top');
-            }
-            scrollPrev = scrolled;
-        });
-    }
-
-    if (window.innerWidth > 991) {
-        headerHide();
-    }
 
 
 
